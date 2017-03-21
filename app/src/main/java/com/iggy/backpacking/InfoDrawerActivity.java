d32e2944d94617ed9fa6a214bf7ed4ea5bb8dc5b
Smith <igor.krstevski.85@googlemail.com>
2017-03-21 20:14:46 +0100

    test

---
+++ app/src/main/java/com/iggy/backpacking/InfoDrawerActivity.java
@@ -0,0 +1,15 @@
+package com.iggy.backpacking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_drawer);
+    }
+}
+
+
